010409ce0564299b054ab85aac0764e303f83049
Michael Watson <michaelwatson747@gmail.com>
2020-10-21 08:57:20 +0100

    Fixed momentum function. Vis returns output.

--- EDA.py
+++ EDA.py
@@ -28,7 +28,7 @@ df.sort_index(level=0, inplace=True)
 df['daily_log_returns'] = df['EndofDayPrice'].apply(np.log).groupby(level=1).diff(1)
 df['daily_mkt_log_returns'] = df['ave_mkt_price'].apply(np.log).groupby(level=1).diff(1) 
 
-first_day = df.loc[('2020-06-06', slice(None)), :]
+first_day = df.loc[('2019-07-01', slice(None)), :]
 top200_mask = first_day.nlargest(200, 'EndofDayPrice').index.droplevel(0).values
 top200 = df.loc[(slice(None), top200_mask), :]
 
@@ -93,7 +93,7 @@ def custom_resampler(resampled_df):
 def find_cutoff(data):
     return data.sort_values()[-15]
 
-def momentum_strat(data, lookback_window=13, holding_period=14, param_dict=None):
+def momentum_strat(data, lookback_window=14, holding_period=14, param_dict=None):
     'Buy top n momentum players from lookback period and hold for duration of holding period'
     # TODO: Val this actually works
     # TODO: FIX THE CUTOFF SITCH (we hold over 10 positions some days. Tie-breaker, highest price???)
@@ -102,32 +102,30 @@ def momentum_strat(data, lookback_window=13, holding_period=14, param_dict=None)
         lookback_window = param_dict['lookback_window']
         holding_period = param_dict['holding_period']
     
-    technicals_df = data['EndofDayPrice'].groupby(level=1).pct_change(lookback_window) # Make momentum factor TODO: USE LOG RETURNS FOR THIS AS WELL.
+    technicals_df = data['EndofDayPrice'].groupby(level=1).pct_change(lookback_window) # Make momentum factor TODO: USE LOG RETURNS FOR THIS AS WELL. (diff???)
     
     # Find 15th place cutoff
-    # technicals_df = technicals_df.unstack().fillna(0)
-    # transposed = technicals_df.transpose()
     cutoff = technicals_df.groupby(level=0).apply(find_cutoff)
     
     # Generate portfolio dataframe
     portfolio_df = technicals_df.unstack()
     portfolio_df[portfolio_df.values >= cutoff.values.reshape(len(cutoff),1)] = 1
     portfolio_df[portfolio_df.values < cutoff.values.reshape(len(cutoff),1)] = 0
+    portfolio_df = portfolio_df[lookback_window+1:] # Slice off the 'warm-up' period
     
     # Fix positions for holding period
     fixed_portfolio = portfolio_df.resample(str(holding_period) + 'D').apply(custom_resampler)
     fixed_portfolio = fixed_portfolio.resample('1D').ffill()
-    fixed_portfolio.iloc[:lookback_window, :] = 0 # Wait for duration of lookback window
+    #fixed_portfolio.iloc[:lookback_window, :] = 0 # Wait for duration of lookback window WAIT EARLIER
 
     # Shift portfolio positions 1 day forward to avoid look-ahead bias
     fixed_portfolio = fixed_portfolio.shift(1)
 
     daily_log_returns = data['daily_log_returns'].unstack()
-    strat_returns = fixed_portfolio * daily_log_returns[:-(holding_period-1)] # TODO: FIX (Chopping off end of price info as upsample wont fill up the end of the dataframe)
+    strat_returns = fixed_portfolio * daily_log_returns[fixed_portfolio.index.min():fixed_portfolio.index.max()]
     strat_returns['cumulative_pf_returns'] = strat_returns.mean(axis=1).cumsum().apply(np.exp)
     
     return strat_returns['cumulative_pf_returns']
-
 
 from sklearn.model_selection import ParameterGrid
 
@@ -163,20 +161,27 @@ def custom_grid_search(data, strategy, param_grid):
 
 
 
-# Testing the funcs out:
-
-
-data = top200
-strategy = momentum_strat
-param_grid = {'lookback_window': [7, 14, 21],
-              'holding_period': [7, 14]}
-
-custom_grid_search(data, strategy, param_grid)
 
 
 # STRATEGY 1: Price momentum
+data = top200
+strategy = momentum_strat
+param_grid = {'lookback_window': [3, 7, 14, 21],
+              'holding_period': [3, 7, 14, 21]}
+
+results = custom_grid_search(data, strategy, param_grid)
+print(results)
+
+optimal_params =  {'holding_period': 7,
+                   'lookback_window': 7}
+
+results = momentum_strat(data, param_dict=optimal_params)
+results.plot(title='Momentum strategy cumulative returns')
+# Check Sharpe ratio?
 
 # STRATEGY 2: Mean reversion
+# Find biggest losers in past x period, and long them. 
+
 
 # STRATEGY 3: Post div. drift
 
